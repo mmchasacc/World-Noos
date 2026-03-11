56cb81e3197dc443b1937fd35b6dd6303420752a
mmchasacc <mehdi.mirzaie@chasacademy.se>
2026-03-11 18:34:24 +0100

    express | Docker setup

---
+++ index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import { Pool } from "pg";
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+app.use(helmet())
+app.use(express.json())
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+    message: "För många anrop, försök igen senare."
+
+})
+app.use(limiter)
+
+app.use(express.static("public"))
+
+app.get('/api/health', async (req, res) => {
+    try {
+        await pool.query('SELECT NOW()')
+        res.json({ status: "OK", message: "Systemet är uppe och databasen svarar."})
+    } catch (err) {
+        res.status(500).json({ status: "Error", message: "Databasfel"})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server körs på http://localhost:${port}`)
+})
